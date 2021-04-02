4f6536b636f8940befe70d3764c82102e001ebd1
Sergey Prokudin <sergey.prokudin@gmail.com>
2021-04-02 16:00:07 +0200

    added custom training

--- demos/train_on_custom.py
+++ demos/train_on_custom.py
@@ -17,6 +17,30 @@ from smplpix.unet import UNet
 from smplpix.training import train, evaluate
 
 
+def generate_eval_video(args, unet, ckpt_path):
+
+    # we will now use the network trained on 20 sketches to convert the rest of AMASS renders
+    print("rendering novel views...")
+    test_dataset = SMPLPixDataset(input_dir=args.input_novel,
+                                  downsample_factor=args.downsample_factor,
+                                  perform_augmentation=False,
+                                  n_input_channels=args.n_input_channels,
+                                  n_output_channels=args.n_output_channels)
+    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size)
+    final_renders_path = os.path.join(args.workdir, 'final_test_renders')
+    _ = evaluate(unet, test_dataloader, final_renders_path, args.device, save_target=False, save_input=True)
+
+    print("generating video animation...")
+    video_animation_path = os.path.join(args.workdir, 'animation.mp4')
+    _ = generate_mp4(final_renders_path, video_animation_path, img_ext='jpeg', frame_rate=15)
+
+    print("all done.")
+    print("network checkpoint: %s" % ckpt_path)
+    print("generated renders: %s" % final_renders_path)
+    print("generated animation video: %s" % video_animation_path)
+
+    return
+
 def main():
 
     print("******************************************************************************************\n"+
@@ -40,30 +64,17 @@ def main():
     unet = UNet(n_channels=args.n_input_channels, n_classes=args.n_output_channels).to(args.device)
 
     print("starting training...")
-    train(model=unet, train_dataloader=dataloader, val_dataloader=dataloader,
-          log_dir=log_dir, ckpt_path=ckpt_path, device=args.device, n_epochs=args.n_epochs,
-          eval_every_nth_epoch=args.eval_every_nth_epoch, sched_patience=args.sched_patience,
-          init_lr=args.learning_rate)
+    try:
 
-    # we will now use the network trained on 20 sketches to convert the rest of AMASS renders
-    print("processing test AMASS renders...")
-    test_dataset = SMPLPixDataset(input_dir=args.input_novel,
-                                  downsample_factor=args.downsample_factor,
-                                  perform_augmentation=False,
-                                  n_input_channels=args.n_input_channels,
-                                  n_output_channels=args.n_output_channels)
-    test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size)
-    final_renders_path = os.path.join(args.workdir, 'final_test_renders')
-    _ = evaluate(unet, test_dataloader, final_renders_path, args.device, save_target=False, save_input=True)
+        train(model=unet, train_dataloader=dataloader, val_dataloader=dataloader,
+              log_dir=log_dir, ckpt_path=ckpt_path, device=args.device, n_epochs=args.n_epochs,
+              eval_every_nth_epoch=args.eval_every_nth_epoch, sched_patience=args.sched_patience,
+              init_lr=args.learning_rate)
+    except KeyboardInterrupt:
+        print("training interrupted...")
+        generate_eval_video(args, unet, ckpt_path)
 
-    print("generating video animation...")
-    video_animation_path = os.path.join(args.workdir, 'animation.mp4')
-    _ = generate_mp4(final_renders_path, video_animation_path, img_ext='jpeg', frame_rate=15)
-
-    print("all done.")
-    print("network checkpoint: %s" % ckpt_path)
-    print("generated renders: %s" % final_renders_path)
-    print("generated animation video: %s" % video_animation_path)
+    generate_eval_video(args, unet, ckpt_path)
 
     return
 
