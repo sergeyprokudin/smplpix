254774b1863621b1472733f1f13673c0b846edf7
Sergey Prokudin <sergey.prokudin@gmail.com>
2021-04-29 09:12:22 +0200

    LICENSE updated

--- smplpix/unet.py
+++ smplpix/unet.py
@@ -1,13 +1,35 @@
 # pytorch U-Net model (https://arxiv.org/abs/1505.04597)
 # original repository:
 # https://github.com/ELEKTRONN
-# (https://github.com/ELEKTRONN/elektronn3/blob/master/elektronn3/models/unet.py)
+# https://github.com/ELEKTRONN/elektronn3/blob/master/elektronn3/models/unet.py
 
 # ELEKTRONN3 - Neural Network Toolkit
 #
 # Copyright (c) 2017 - now
 # Max Planck Institute of Neurobiology, Munich, Germany
 # Author: Martin Drawitsch
+
+# MIT License
+#
+# Copyright (c) 2017 - now, ELEKTRONN team
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 """
 This is a modified version of the U-Net CNN architecture for biomedical
