fafe9416065e617a459c539a729dfeb4391c5630
Sergey Prokudin <sergey.prokudin@gmail.com>
2021-04-30 09:28:59 +0200

    eval script fix

--- smplpix/args.py
+++ smplpix/args.py
@@ -15,6 +15,7 @@ def get_smplpix_arguments():
                         default=None)
     parser.add_argument('--resume',
                         dest='resume',
+                        type=bool,
                         help='whether to continue training process given the checkpoint in workdir',
                         default=False)
     parser.add_argument('--data_url',
